b6f5f331bc66ca5f017cc4805c36e9ee6b899c1a
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2018-08-11 15:04:57 +0530

    Add files via upload

---
+++ MinimizeNumberOFMoves.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>arr[i];
+		
+		sort(arr,arr+n);
+		int ans=0;
+		for(int i=0;i<n;i++)
+		{
+			ans+=arr[i]-arr[0];
+		}
+		cout<<"\n"<<ans<<"\n";
+			
+	}
+}
+
