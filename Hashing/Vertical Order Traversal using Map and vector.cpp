82bb741c8f9eb268598486031a9fab4b9287ae55
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2018-08-26 23:26:14 +0530

    Add files via upload

---
+++ Hashing/Vertical Order Traversal using Map and vector.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+struct Node
+{	int data;
+	struct Node *left,*right;
+};
+//O(nlogn) as map takes logn time
+Node * newNode(int data)
+{
+	Node *temp = new Node;
+	temp->data=data;
+	temp->right=temp->left=NULL;
+	return temp;
+}
+void getVerticalOrder(Node *root,int hd, map<int, vector<int> > &m)
+{
+	if(root == NULL)
+		return ;
+	m[hd].push_back(root->data);
+	
+	getVerticalOrder(root->left,hd-1,m);
+	getVerticalOrder(root->right,hd+1,m);
+}
+void printVerticalOrder(Node *root)
+{
+	map<int ,vector<int> > m;
+	int hd=0;
+	getVerticalOrder(root,hd,m);
+	//map<int ,vector<int> >  :: iterater it;
+	map< int,vector<int> > :: iterator it;
+	for(it=m.begin();it!=m.end();it++)
+	{
+		for(int i=0; i<it->second.size(); i++)
+			cout<<it->second[i]<<"  ";
+		cout<<endl;
+	}
+}
+int main()
+{
+	Node *root = newNode(1);
+	root->left=newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right= newNode(7);
+	root->right->left->right = newNode(8);
+	root->right->right->right = newNode(9);
+	
+	cout<<"Vertical Order traversal is\n";
+	printVerticalOrder(root);
+	return 0;
+}
