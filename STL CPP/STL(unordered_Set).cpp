e6a12b04238a69bbf8b0ec42503ad6934b71bad7
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-01-30 20:53:59 +0530

    Add files via upload

---
+++ STL CPP/STL(unordered_Set).cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	map<char ,int > M;
+	//unordered_map<char ,int > U;
+	unordered_map<char , int > m1;
+	string s="Rahul Srivastav";
+	for(char c : s)	M[c]++;
+	for(char c : s)	//U[c]++;
+}
