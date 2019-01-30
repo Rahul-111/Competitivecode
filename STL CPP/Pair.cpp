51341aec80ebdfb76e49d37b8d26272bf9f3256e
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-01-30 20:53:32 +0530

    Add files via upload

---
+++ STL CPP/Pair.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	pair<int , char > p;
+	p=make_pair(2,'a');
+	cout<<p.first<< " "<<p.second<<endl;
+	pair<int ,char > p2(1,'a');
+	cout<<p2.first<<" "<<p2.second<<endl;
+	return 0;
+}
