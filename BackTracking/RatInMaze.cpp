643615e8fa5504e990ddc6c62fd20c21fff3291d
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-02-10 18:42:11 +0530

    Add files via upload

---
+++ BackTracking/RatInMaze.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+static int cnt=0;
+void printPathHelper(int** input,int n,int** solution,int x,int y){
+	if(x==n-1 && y==n-1)
+	{
+		solution[x][y]=1;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				cout<<solution[i][j]<<" ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+		cnt++;
+	}
+	if(x<0 || y<0 || x>=n || y>=n ||input[x][y]==0 || solution[x][y]==1){
+		return ;
+	}
+	solution[x][y]=1;
+	
+	printPathHelper(input,n,solution,x+1,y);
+	printPathHelper(input,n,solution,x-1,y);
+	printPathHelper(input,n,solution,x,y+1);
+	printPathHelper(input,n,solution,x,y-1);
+	
+	solution[x][y]=0;
+}
+bool hasPathHelper(int** input,int n,int** solution,int x,int y){
+	if(x==n-1 && y==n-1){
+		return true;
+	}
+	if(x<0 || y<0 || x>=n || y>=n ||input[x][y]==0 || solution[x][y]==1){
+		return false;
+	}
+	solution[x][y]=1;
+	
+	if(hasPathHelper(input,n,solution,x-1,y))
+		return true;
+	if(hasPathHelper(input,n,solution,x+1,y))
+		return true;
+	if(hasPathHelper(input,n,solution,x,y-1))
+		return true;
+	if(hasPathHelper(input,n,solution,x,y+1))
+		return true;
+	solution[x][y]=0;
+	return false;
+}
+bool hasPath(int** arr,int n){
+	int** solution = new int*[n];
+	for(int i=0;i<n;i++){
+		solution[i] = new int[n];
+		for(int j=0;j<n;j++)
+			solution[i][j]=0;
+	} 
+	printPathHelper(arr,n,solution,0,0);
+	return true; 
+	//return hasPathHelper(arr,n,solution,0,0);
+}
+int main(){
+	int **arr;
+	int n;
+	cin>>n;
+	arr = new int*[n];
+	for(int i=0;i<n;i++){
+		arr[i]=new int[n];
+		for(int j=0;j<n;j++){
+			cin>>arr[i][j];
+		}
+	}
+	if(hasPath(arr,n)){
+		cout<<cnt<<" Path exist"<<endl;
+	}
+	else
+		cout<<"Not a possible solution"<<endl;
+	return 0;
+}
