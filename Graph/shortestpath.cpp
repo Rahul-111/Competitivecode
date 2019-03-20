e6c4ce061dcae2fd2e96d2f918203072196de041
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-03-20 17:13:23 +0530

    Add files via upload

---
+++ Graph/shortestpath.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph{
+	int V;
+	list<int > *adjList;
+	public:
+	Graph(int v){
+		V=v;
+		adjList = new list<int >[V];
+	}
+	void addEdge(int u,int v,bool bidir=true){
+		adjList[u].push_back(v);
+		if(bidir){
+			adjList[v].push_back(u);
+		}
+	}
+	void printAdjList(){
+		for(int i=0;i<V;i++){
+			cout<<i<<"->";
+			for(int node:adjList[i]){
+				cout<<node<<",";
+			}
+			cout<<endl;
+		}
+	}
+	void bfs(int src,int dest){
+		queue<int > q;
+		bool *visited = new bool[V]{0};
+		int *distance=new int[V]{0};
+		int *parent=new int[V];
+		for(int i=0;i<V;i++){
+			parent[i]=-1;
+		}
+		q.push(src);
+		visited[src]=true;
+		distance[src]=0;
+		while(!q.empty()){
+			int node=q.front();
+			cout<<node<<" ";
+			q.pop();
+			
+			for(int neighbour:adjList[node]){
+				if(!visited[neighbour]){
+					q.push(neighbour);
+					visited[neighbour]=true;
+					distance[neighbour]=distance[node]+1;
+					parent[neighbour]=node;
+				}
+			}
+		}
+		cout<<endl;
+		for(int i=0;i<V;i++){
+			cout<<i<<" node having dist "<<distance[i]<<endl;
+		}
+		
+		cout<<"Shortest distance is "<<distance[dest]<<endl;
+		cout<<"Shortest path is ";
+		int temp=dest;
+		while(temp!=-1){
+			cout<<temp<<"<--";
+			temp=parent[temp];
+		}
+	}
+};
+int main(){
+	Graph g(6);
+	g.addEdge(0,1);
+	g.addEdge(1,2);
+	g.addEdge(0,4);
+	g.addEdge(2,4);
+	g.addEdge(3,4);
+	g.addEdge(3,2);
+	g.addEdge(3,5);
+	
+	g.printAdjList();
+	cout<<"searched nodes"<<endl;
+	g.bfs(0,5);
+	return 0;
+}
