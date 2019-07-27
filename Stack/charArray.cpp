7b38bcbb692fc6a8f82ff3763c7ad689fece4dab
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-07-27 18:04:53 +0530

    Add files via upload

---
+++ Stack/charArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		char s[1000];
+		cin>>s;
+		int n=sizeof(s)/sizeof(s[0]);
+		cout<<"its size is "<<n<<endl;
+	}
+	return 0;
+}
