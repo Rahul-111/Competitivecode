11d5a6a0605341fd310b480692d2730f8e72025c
Rahul-111 <36945601+Rahul-111@users.noreply.github.com>
2019-07-27 18:07:32 +0530

    Add files via upload

---
+++ Stack/minELementInO(1).cpp
@@ -0,0 +1,71 @@
+//algo for find min in O(1) time and space complexity
+//push
+//1. if stack is empty insert x,minElement<-x
+//2.if stack is non empty compare x with minElement
+//3a. x>=minELement insert x
+//3b. x<minElement then insert 2*x-minElement , minElement<-x;
+//
+//
+//Pop
+//1.Remove element form y<-top
+//2. y>=minElement minELement<-minElement (continue) 
+//3. y<minElement do minElement = 2*minElement -y
+#include<bits/stdc++.h>
+using namespace std;
+class Stack{
+	stack<int > s;
+	int minElement;
+	public:
+		void push(int x){
+			if(s.size()==0){
+				s.push(x);
+				minElement=x;
+			}	
+			else{
+				if(x>=minElement){
+					s.push(x);
+				}
+				else{
+					s.push(2*x-minElement);
+					minElement = x;
+				}
+			}
+		}
+		int pop(){
+			if(s.size()==0)
+				return -1;
+			int y = s.top();
+			s.pop();
+			if(y>=minElement)
+				minElement = minElement;
+			else{
+				minElement = 2*(this->minElement) - y;
+			}
+			return y;
+		}
+		int getMin(){
+			if(s.size()==0){
+				return -1;
+			}
+			return minElement;
+		}
+};
+int main(){
+	Stack k;
+	k.push(18);
+	k.push(19);
+	k.push(29);
+	k.push(15);
+	k.push(16);
+	
+	cout<<"Min "<<k.getMin()<<endl;
+	int x=k.pop();
+	cout<<"Min = "<<k.getMin()<<endl;
+	
+	x=k.pop();
+	cout<<"Min = "<<k.getMin()<<endl;
+	
+	x=k.pop();
+	cout<<"Min = "<<k.getMin()<<endl;
+	return 0;
+}
